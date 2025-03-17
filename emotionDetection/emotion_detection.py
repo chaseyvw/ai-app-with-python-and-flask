dbeb51d0fcf4e6f3fdd6692d3a2fc779ae0c7ee5
chaseylorraine <chaseyvw@gmail.com>
2025-03-17 16:51:43 -0700

    commit

--- emotionDetection/emotion_detection.py
+++ emotionDetection/emotion_detection.py
@@ -1,14 +1,13 @@
 import requests
 import json
 
-def emotion_detection(textToAnalyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+def emotion_detector(text_to_analyze):
+    url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = { "raw_document": { "text": textToAnalyze } }
-
+    input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=input_json, headers=header)
     status_code = response.status_code
-    
+
     emotions = {}
 
     if status_code == 200:
